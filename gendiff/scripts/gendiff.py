4ad50c172eb0d8bd7c89dcdceaa43e346b9b687d
AlexMusin <alersky21@gmail.com>
2022-07-06 18:03:26 +0300

    Update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,8 +2,9 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-parser.add_argument(metavar='first_file', dest = 'accumulate')
-parser.add_argument(metavar='second_file', dest = 'accumulate')
+parser.add_argument(metavar='first_file', dest='first_file')
+parser.add_argument(metavar='second_file', dest='second_file')
+parser.add_argument('-f', '--format', dest='format', help='set format of output')
 args = parser.parse_args()
 print(args.accumulate(args.integers))
 
