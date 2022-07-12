4625b41c784f5ad1c6ee9578a1c561338fb03c97
AlexMusin <alersky21@gmail.com>
2022-07-12 18:30:46 +0300

    3rd step

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/AlexMusin/python-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/AlexMusin/python-project-lvl2/actions)
+
+
+3rd step asciinema:
+https://asciinema.org/a/O7PNd8aoat5E9frWohSZnLXYj
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,17 +1,66 @@
 import argparse
-
+import json
 
 parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-parser.add_argument(metavar='first_file', dest='first_file')
-parser.add_argument(metavar='second_file', dest='second_file')
+parser.add_argument('first_file_path')
+parser.add_argument('second_file_path')
 parser.add_argument('-f', '--format', dest='format', help='set format of output')
+parser.add_argument('-V', '--version', help='set format output the version number')
 args = parser.parse_args()
-print(args.accumulate(args.integers))
+#print(args.first_file_path)
+
+def generate_diff(first_file_path, second_file_path):
+    with open(first_file_path, 'r') as f:
+        first_file = json.load(f)
+    with open(second_file_path, 'r') as f:
+        second_file = json.load(f)
+    sorted_keys = sorted(list(set(list(first_file.keys()) + list(second_file.keys()))))
+    out_string = '{'
+    for key in sorted_keys:
+        if key in first_file and key in second_file:
+            if first_file[key] == second_file[key]:
+                out_string += (
+                    '\n    '
+                    + json.dumps(key).strip('"')
+                    + ': '
+                    + json.dumps(first_file[key]).strip('"')
+                    )
+            else:
+                out_string += (
+                    '\n  - '
+                    + json.dumps(key).strip('"')
+                    + ': '
+                    + json.dumps(first_file[key]).strip('"')
+                    )
+                out_string += (
+                    '\n  + '
+                    + json.dumps(key).strip('"')
+                    + ': '
+                    + json.dumps(second_file[key]).strip('"')
+                    )
+        elif key not in second_file:
+            out_string += (
+                    '\n  - '
+                    + json.dumps(key).strip('"')
+                    + ': '
+                    + json.dumps(first_file[key]).strip('"')
+                    )
+        elif key not in first_file:
+            out_string += (
+                    '\n  + '
+                    + json.dumps(key).strip('"')
+                    + ': '
+                    + json.dumps(second_file[key]).strip('"')
+                    )
+    out_string += '\n}'
+    return out_string
+
+
 
 
 
 def main():
-    print('aaa')
+    print(generate_diff(args.first_file_path, args.second_file_path))
 
 
 if __name__ == '__main__':
