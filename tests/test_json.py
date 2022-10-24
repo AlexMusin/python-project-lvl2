ea73fa1c59bd2676e91a045f4e833a6becd2e376
AlexMusin <alersky21@gmail.com>
2022-10-24 23:42:21 +0300

    add diff_builder.py and tests for it

---
+++ tests/test_json.py
@@ -0,0 +1,29 @@
+import os
+from gendiff.scripts.gendiff import generate_diff
+
+def test_normal_case():
+    with open('tests/fixtures/file_expected_format_stylish', 'r') as f:
+        expected = f.read()
+        assert generate_diff(
+            'tests/fixtures/file1.json',
+            'tests/fixtures/file2.json'
+            ) == expected
+        
+
+def test_both_empty_case():
+    assert generate_diff(
+            'tests/fixtures/flat_file2_empty.yml',
+            'tests/fixtures/flat_file2_empty.yml'
+            ) == '{\n}'
+
+
+def test_single_empty_case():
+    with open(
+        'tests/fixtures/file_empty_expected_format_stylish',
+        'r'
+        ) as f:
+        expected = f.read()        
+        assert generate_diff(
+            'tests/fixtures/file1.json',
+            'tests/fixtures/flat_file2_empty.json'
+            ) == expected
