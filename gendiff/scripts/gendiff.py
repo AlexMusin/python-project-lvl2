d51e8b14c8516cb03c924dc1501f6ed076a1d089
AlexMusin <alersky21@gmail.com>
2022-11-03 21:39:11 +0300

    modify gendiff arguments

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -24,13 +24,13 @@ def generate_diff(first_file_path, second_file_path, format):
 def main():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file_path')
-    parser.add_argument('second_file_path')
     parser.add_argument(
         '-f', '--format',
         type=str,
         default='stylish',
         help='set format of output')
+    parser.add_argument('first_file_path')
+    parser.add_argument('second_file_path')
     args = parser.parse_args()
     format = args.format
     print(generate_diff(args.first_file_path, args.second_file_path, format))
