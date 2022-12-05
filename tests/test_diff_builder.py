dbac593cfe68facecb44c056bf4ac83a3f646515
AlexMusin <alersky21@gmail.com>
2022-12-05 20:39:44 +0300

    linter fixes

--- tests/test_diff_builder.py
+++ tests/test_diff_builder.py
@@ -6,8 +6,10 @@ from gendiff.files_input import read_files
 def test_normal_case():
     with open('tests/fixtures/file_expected_diff_normal.txt') as f:
         expected = f.read()
-        files_read = read_files('tests/fixtures/file1_smaller.json', 'tests/fixtures/file2_smaller.json')
-        #print(files_read)
+        files_read = read_files(
+            'tests/fixtures/file1_smaller.json',
+            'tests/fixtures/file2_smaller.json'
+        )
         files_parsed = parse_files(files_read)
         diff = dict_diff(files_parsed)
         assert str(diff) == expected
