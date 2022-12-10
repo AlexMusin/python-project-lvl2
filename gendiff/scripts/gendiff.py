1e5f8aaa39b8fed15c344bb33f99ffcb16d20068
AlexMusin <alersky21@gmail.com>
2022-12-11 00:56:49 +0300

    insert cli func into gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,38 @@
-from gendiff import diff_generator
+import argparse
+from gendiff.diff_generator import generate_diff
+
+
+FORMAT_TUP = (
+    'stylish',
+    'plain',
+    'json',
+)
+
+
+def cli():
+    '''Launch difference building and return it with defined formater'''
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
+    parser.add_argument(
+        '-f', '--format',
+        type=str,
+        default='stylish',
+        help='set format of output')
+    parser.add_argument('first_file_path')
+    parser.add_argument('second_file_path')
+    args = parser.parse_args()
+    format = args.format
+    first_file_path = args.first_file_path
+    second_file_path = args.second_file_path
+    if format not in FORMAT_TUP:
+        print('''User defined format is not available.
+        Format switched to "stylish"''')
+        format = 'stylish'
+    print(generate_diff(first_file_path, second_file_path, format))
 
 
 def main():
-    print(diff_generator.cli())
+    cli()
 
 
 if __name__ == '__main__':
