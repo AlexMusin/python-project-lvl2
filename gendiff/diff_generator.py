ed72980c1e0740df67905590556d088152fe067f
AlexMusin <alersky21@gmail.com>
2022-12-11 01:14:34 +0300

    move format validator func to diff_generator.py

--- gendiff/diff_generator.py
+++ gendiff/diff_generator.py
@@ -4,7 +4,29 @@ from gendiff.files_parser import parse_files
 from gendiff.diff_out import diff_output
 
 
+FORMAT_TUP = (
+    'stylish',
+    'plain',
+    'json',
+)
+
+
+def validate_format(format):
+    if format not in FORMAT_TUP:
+        raise Exception(
+            '''
+            Entered format is not valid
+            Please choose one of formats below:
+            - stylish
+            - plain
+            - json
+            Default: stylish
+            '''
+        )
+
+
 def generate_diff(first_file_path, second_file_path, format='stylish'):
+    validate_format(format)
     files = read_files(first_file_path, second_file_path)
     parsed_files = parse_files(files)
     difference = diff_builder.dict_diff(parsed_files)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,13 +2,6 @@ import argparse
 from gendiff.diff_generator import generate_diff
 
 
-FORMAT_TUP = (
-    'stylish',
-    'plain',
-    'json',
-)
-
-
 def cli():
     '''Launch difference building and return it with defined formater'''
     parser = argparse.ArgumentParser(
@@ -24,10 +17,6 @@ def cli():
     format = args.format
     first_file_path = args.first_file_path
     second_file_path = args.second_file_path
-    if format not in FORMAT_TUP:
-        print('''User defined format is not available.
-        Format switched to "stylish"''')
-        format = 'stylish'
     print(generate_diff(first_file_path, second_file_path, format))
 
 
