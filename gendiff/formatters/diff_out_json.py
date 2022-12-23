87d9f8f23eb8e466f82f141b377b949574189c68
AlexMusin <alersky21@gmail.com>
2022-12-23 19:35:49 +0300

    remove sort from diff_out_json.py

--- gendiff/formatters/diff_out_json.py
+++ gendiff/formatters/diff_out_json.py
@@ -4,6 +4,5 @@ from gendiff import diff_inner_representation as ig
 
 def out(inp_diff):
     '''Return difference in JSON format with indentation'''
-    sorted_diff = sorted(inp_diff, key=lambda k: ig.get_name(k))
-    out = json.dumps(sorted_diff, indent=2)
+    out = json.dumps(inp_diff, indent=2)
     return out
